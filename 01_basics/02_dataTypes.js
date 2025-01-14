e7cfe388144b5d49212bc80ec129edaddff13568
Zeeshan Riasat <zeehan.riasat@planlabsolutions.com>
2025-01-14 11:36:43 +0500

    data types

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,3 +2,19 @@
 
 // alert(3+3) // we are using node js, not browser
 
+// code readability should be high.
+
+// data types
+
+// number => 2 ^ 53
+// bigint
+// string => ""
+// boolean => trye/false
+// null => standalone value
+// undefined
+// symbol => used for uniqueness
+
+// objects
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
